fcfc0826c620dd133cae3a92331671abb744bc6d
Paulo Eduardo Fagundes dos Santos <paulo.esantos@gmail.com>
2014-06-27 22:15:35 -0300

    Resultado
    resultado dos códigos

--- Exercicio005/tex_async.js
+++ Exercicio005/tex_async.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 for (var i = 1; i <= 5; i++) {
-	var file = "sync-txt" + i + ".txt";
+	var file = "async-txt" + i + ".txt";
 	fs.writeFile(file, "Hello Node.js!", function(err, out) {
 		console.log(out);
 	});
