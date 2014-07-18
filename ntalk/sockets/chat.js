3a5ae51d68205fe1088cfa13c802a4c58bdb82e9
PauloFagundes <paulo.esantos@gmail.com>
2014-07-18 16:05:59 -0300

    6.6
    gerenciando salas de chat

--- ntalk/sockets/chat.js
+++ ntalk/sockets/chat.js
@@ -1,12 +1,38 @@
 module.exports = function(io) {
-    var sockets = io.sockets;
+    var crypto = require('crypto'),
+        md5 = crypto.createHash('md5'),
+        sockets = io.sockets;
     sockets.on('connection', function(client) {
         var session = client.handshake.session,
             usuario = session.usuario;
+        client.on('join', function(sala) {
+            if (sala) {
+                sala = sala.replace('?', '');
+            } else {
+                var timestamp = new Date().toString();
+                var md5 = crypto.createHash('md5');
+                sala = md5.update(timestamp).digest('hex');
+            }
+            client.set('sala', sala);
+            client.join(sala);
+        });
+
+        client.on('disconnect', function() {
+            client.get('sala', function(erro, sala) {
+                client.leave(sala);
+            });
+        });
+
         client.on('send-server', function(msg) {
             var msg = "<b>" + usuario.nome + ":</b>" + msg + "<br>";
-            client.emit('send-client', msg);
-            client.broadcast.emit('send-client', msg);
+            client.get('sala', function(erro, sala) {
+                var data = {
+                    email: usuario.email,
+                    sala: sala
+                };
+                client.broadcast.emit('new-message', data);
+                sockets.in(sala).emit('send-client', msg);
+            });
         });
     });
 }
