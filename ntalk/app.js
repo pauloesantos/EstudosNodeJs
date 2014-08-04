4fafb4ccf7c7100d5239103760f08aea2118f0ed
PauloFagundes <paulo.esantos@gmail.com>
2014-08-04 13:05:56 -0300

    Final

--- ntalk/app.js
+++ ntalk/app.js
@@ -11,7 +11,14 @@ var express = require('express'),
 
 
 const SECRET = 'Ntalk',
-    KEY = 'ntalk.sid';
+    KEY = 'ntalk.sid',
+    MAX_AGE = {
+        maxAge: 3600000
+    },
+    GZIP_LVL = {
+        level: 9,
+        memLevel: 9
+    };
 var cookie = express.cookieParser(SECRET),
     storeOpts = {
         client: redis.getClient(),
@@ -25,21 +32,24 @@ var cookie = express.cookieParser(SECRET),
     },
     session = express.session(sessOpts);
 
+app.use(express.logger('dev'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(cookie);
 app.use(session);
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.compress(GZIP_LVL));
 app.use(app.router);
-
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/public', MAX_AGE));
 app.use(error.notFound);
 app.use(error.serverError);
 
-// ... stack de configurações do servidor..
-
+io.enable('browser client cache');
+io.enable('browser client minification');
+io.enable('browser client etag');
+io.enable('browser client gzip');
+io.set('log level', 1);
 io.set('store', new SocketStore);
 io.set('authorization', function(data, accept) {
     cookie(data, {}, function(err) {
